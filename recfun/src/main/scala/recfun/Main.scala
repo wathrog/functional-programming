dfe34886137fa6840bc61886b16a16a6290cd4e2
Francesco Salbaroli <francesco.salbaroli@gmail.com>
2013-03-26 22:56:34 +0100

    2 exercises done of 1st week assignment

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,12 +14,40 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c < 0) throw new NoSuchElementException
+    if (r < 0) throw new NoSuchElementException
+    if (c > r) throw new NoSuchElementException
+    pascalRec(c, r)
+  }
+  
+  def pascalRec(c: Int, r: Int): Int = {
+    c match {
+      case 0 => 1
+      case `r` => 1
+      case _ => pascal(c-1,r-1) + pascal(c,r-1)
+    }
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    balanceInt(chars, 0) == 0
+  }
+
+  def balanceInt(chars: List[Char], counter: Int): Int = {
+    if (chars.isEmpty)
+      counter
+    else {
+      chars.head match {
+        case '(' => balanceInt(chars.tail, counter + 1)
+        case ')' if counter > 0 => balanceInt(chars.tail, counter - 1)
+        case ')' if counter == 0 => -1
+        case _ => balanceInt(chars.tail, counter)
+      }
+    }
+  }
 
   /**
    * Exercise 3
