3c3fd8788c068c135476dc6f16df2b4e15ee929f
Francesco Salbaroli <francesco.salbaroli@gmail.com>
2013-04-10 23:33:13 +0200

    all minus one assignment

--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,10 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  println(contains(singletonSet(2), 1))
+  val unionSet = union(singletonSet(1), singletonSet(2))
+  val filteredSet = filter(unionSet, (x : Int) => x <= 3);
+  println(contains(filteredSet, 3))
+  println(forall(unionSet, (x : Int) => x <= 1))
+  println(exists(unionSet, (x : Int) => x > 2))
 }
